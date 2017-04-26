fd01770148a8f99436f139f3dd922777e56ac4b5
Thao <ots.hunter@gmail.com>
2017-04-26 17:34:09 +0700

    added some res and commit before fuck up everything

--- app/src/main/java/com/example/tonto/zees/MainActivity.java
+++ app/src/main/java/com/example/tonto/zees/MainActivity.java
@@ -460,15 +460,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 }
             });
             builderSingle.show();
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_custom) {
 
         }
 
