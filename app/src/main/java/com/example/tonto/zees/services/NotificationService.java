4dd1429de47d3688fe0b9df5d60864c7e6af76c6
Le Minh Hoang <le.minhhoang@outlook.com>
2017-05-10 00:13:09 +0700

    Timer cancel when swipe from task.

--- app/src/main/java/com/example/tonto/zees/services/NotificationService.java
+++ app/src/main/java/com/example/tonto/zees/services/NotificationService.java
@@ -1,10 +1,15 @@
 package com.example.tonto.zees.services;
 
+import android.app.AlarmManager;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+
+import com.example.tonto.zees.receivers.TimerReceiver;
 
 /**
  * Created by Hoang on 5/4/2017.
@@ -22,5 +27,10 @@ public class NotificationService extends Service {
         super.onTaskRemoved(rootIntent);
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.cancel(0);
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(getApplicationContext(), TimerReceiver.class);
+        PendingIntent timerIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+        timerIntent.cancel();
+        alarmManager.cancel(timerIntent);
     }
 }
